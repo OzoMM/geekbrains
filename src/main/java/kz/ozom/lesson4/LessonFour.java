e8cef5c56df8f7cc04a7a3f20fc5d55cca9138e7
Azamat Toktarbayev <a.toktarbayev@gmail.com>
2021-02-01 23:42:52 +0600

    GitHUB

--- src/main/java/kz/ozom/lesson4/LessonFour.java
+++ src/main/java/kz/ozom/lesson4/LessonFour.java
@@ -17,12 +17,85 @@ public class LessonFour {
 
     public static void main(String[] args){
         initMap();
-        showMap();
+        int odd = rand.nextInt(2);
+        while (true){
+            odd++;
+            if (odd % 2 == 0) {
+                humanTurn();
+                if (isWin(DOT_X)){
+                    showMap();
+                    System.out.println("Выиграл человек");
+                    break;
+                }
+            }
+            else {
+                aiTurn();
+                if (isWin(DOT_O)){
+                    showMap();
+                    System.out.println("Выиграл компьтер");
+                    break;
+                }
+            }
+            showMap();
+            if (isMapFull()){
+                System.out.println("Ничья!");
+            }
+        }
+    }
 
-        humanTurn();
-        showMap();
-        aiTurn();
-        showMap();
+    private static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isWin(char dot) {
+        // Проверка вертикали и горизонтали
+        for (int i = 0; i < SIZE; i++) {
+            int winI = 0;
+            int winJ = 0;
+            for (int j = 0; j < SIZE; j++) {
+                winI += map[i][j] == dot ? 1 : winI == 0 ? 0 : -1;
+                winJ += map[j][i] == dot ? 1 : winJ == 0 ? 0 : -1;
+                if ((winI == DOTS_TO_WIN) || (winJ == DOTS_TO_WIN)){
+                    return true;
+                }
+            }
+        }
+
+        // Проверка главных диагоналей
+        int winI = 0;
+        int winJ = 0;
+        for (int i = 0; i < SIZE; i++) {
+            winI += map[i][i] == dot ? 1 : winI == 0 ? 0 : -1;
+            winJ += map[i][SIZE-(i+1)] == dot ? 1 : winJ == 0 ? 0 : -1;
+            if ((winI == DOTS_TO_WIN) || (winJ == DOTS_TO_WIN)){
+                return true;
+            }
+        }
+
+        // Проверка побочных диагоналей
+        winI = 0;
+        winJ = 0;
+        int winK = 0;
+        int winL = 0;
+        for (int i = 0; i < SIZE - 1; i++) {
+            winI += map[i][i+1] == dot ? 1 : winI == 0 ? 0 : -1;
+            winJ += map[i+1][i] == dot ? 1 : winJ == 0 ? 0 : -1;
+            winK += map[i][SIZE - 2 - i] == dot ? 1 : winK == 0 ? 0 : -1;
+            winL += map[i+1][SIZE - 1 - i] == dot ? 1 : winL == 0 ? 0 : -1;
+
+            if ((winI == DOTS_TO_WIN) || (winJ == DOTS_TO_WIN) || (winK == DOTS_TO_WIN) || (winL == DOTS_TO_WIN)){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static void humanTurn() {
@@ -39,9 +112,7 @@ public class LessonFour {
     private static void aiTurn() {
         int x, y;
         do {
-            System.out.println("Введите координату X");
             x = rand.nextInt(SIZE);
-            System.out.println("Введите координату Y");
             y = rand.nextInt(SIZE);
         } while(!isTurnValid(x, y));
         map[x][y] = DOT_O;
