96d320f085d48b44065f540964b71b4f5392f63f
Azamat Toktarbayev <a.toktarbayev@gmail.com>
2021-03-19 21:25:15 +0600

    GitHUB

--- src/main/java/kz/ozom/lesson14/LessonFourteen.java
+++ src/main/java/kz/ozom/lesson14/LessonFourteen.java
@@ -3,6 +3,8 @@ package kz.ozom.lesson14;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+
 public class LessonFourteen {
 
     private static final Logger LOGGER = LogManager.getLogger(LessonFourteen.class);
@@ -15,6 +17,26 @@ public class LessonFourteen {
         LOGGER.error("Error");
         LOGGER.fatal("Fatal");
         LOGGER.info("String: {}.", "Hello, World");
+
+    }
+
+    public static void checkArray(ArrayList arrayList) throws FourIntException {
+        int lastPosition = 0;
+        try {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i).equals(4)){
+                    lastPosition = i;
+                }
+                if (lastPosition == 0) {
+                    throw new FourIntException();
+                }
+            }
+        } catch (FourIntException e){
+            System.out.println("Нет ни одной четверки");
+        }
+    }
+
+    public static class FourIntException extends Exception {
 
     }
 }
