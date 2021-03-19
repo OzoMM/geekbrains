09cf5308e05786e07869c4536e6dcc41ad35dd10
Azamat Toktarbayev <a.toktarbayev@gmail.com>
2021-03-19 19:29:10 +0600

    GitHUB

---
+++ src/main/java/kz/ozom/lesson14/LessonFourteen.java
@@ -0,0 +1,15 @@
+package kz.ozom.lesson14;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LessonFourteen {
+
+    private static final Logger LOGGER = LogManager.getLogger(LessonFourteen.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.trace("ddd");
+
+    }
+}
